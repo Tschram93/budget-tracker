fa6bd7bd40e6be7cf582f2ed7e894bee2187459a
TS93 <tschram93@yahoo.com>
2021-12-11 17:54:09 -0500

    connected with heroku

--- server/server.js
+++ server/server.js
@@ -7,19 +7,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // include mongoose here when set up
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/3000', {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+});
 
 // Include route
 app.use(require(route));
 
 app.listen(PORT, () => {
-    console.log(`App running on localhost:${PORT}!`);
+	console.log(`App running on localhost:${PORT}!`);
 });
