54e9d30806f8ebeed51ef9be9c61c7b95c5271e4
TS93 <tschram93@yahoo.com>
2021-12-11 14:29:46 -0500

    installed modules setting up server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const route = './routes/api.js';
 const app = express();
 
 const PORT = process.env.PORT || 3001
 
 
-
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+// include mongoose here when set up
+
+
+// Include route
+app.use(require(route));
+
+app.listen(PORT, () => {
+    console.log(`App running on localhost:${PORT}!`);
+});
